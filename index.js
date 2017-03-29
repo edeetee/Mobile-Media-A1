74a7bcb18bc866d4a31caf8144bb66d6a6e205f8
Edward Taylor <edeetee@gmail.com>
2017-03-30 00:52:47 +1300

    started to toy around with layout etc, tried to get js libraries working

---
+++ index.js
@@ -0,0 +1,12 @@
+$(function(){
+	// var items = $('.pin');
+	// $window.scroll(function(){
+	// 	for (var i = 0; i < items.length; i++) {
+	// 		var item = items[i];
+	// 		var x = item.offset.top + item.height / 2;
+	// 		if(x < window.w){
+				
+	// 		}
+	// 	}
+	// });
+})
--- index.md
+++ index.md
@@ -1,1 +1,3 @@
 ## Mobile Media Assignment 1
+
+#[assignment](index.html)
